69bf69542e8964f066764c5aeb75c2d407dbdb8e
Ben Christel <ben.christel@gmail.com>
2015-09-05 14:58:10 -0700

    automatically parse links

--- preprocess.rb
+++ preprocess.rb
@@ -24,7 +24,7 @@ while line = template.gets
     Preprocessor.new(input).each_processed_line do |line|
       markdown += line
     end
-    markdown_converter = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown_converter = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
     output << markdown_converter.render(markdown)
   else
     output << line
